979e58c163f16de4e5b05e86c85678215d398435
Alex Mehr <alirezaalipouri@gmail.com>
2018-07-13 12:24:12 -0700

    creating a customer purchase inquiry

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # MySQL-homework
+
++ mysql@2.15.0
+added 11 packages in 1.914s
+ alirezamehr@Alirezas-MacBook-Pro  ~/Documents/Bootcamp/MySQL-homework   master  node bamazonCustomer.js
+^C
+ ✘ alirezamehr@Alirezas-MacBook-Pro  ~/Documents/Bootcamp/MySQL-homework   master  node bamazonCustomer.js
+^C
+ ✘ alirezamehr@Alirezas-MacBook-Pro  ~/Documents/Bootcamp/MySQL-homework   master  node bamazonCustomer.js
+connected as id 13
---
+++ bamazonCustomer.js
@@ -0,0 +1,112 @@
+var mysql = require("mysql");
+// var inquirer =require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    port: 3306,
+
+    user: "root",
+
+    password: "Am450478",
+    database: "bamazon"
+});
+
+connection.connect(function (err) {
+    console.log("connected as id " + connection.threadId);
+    if (err) throw err;
+    
+    loadProducts();
+
+});
+
+function loadProducts() {
+    
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+            console.table(res);
+
+        promptCustomerForItem(res);
+    });
+}
+
+function promptCustomerForItem(inventory){
+    inquirer
+       .prompt([
+           {
+               name: "choice",
+               type: "input",
+               message: "please enter the id for an item you would like to purchase.",
+               
+               validate: function(val){
+                   return !isNaN(val);
+               }
+
+           }
+       ]) 
+       
+    .then(function(val) {
+        var choiceId = parseInt(val.choice);
+        var product = checkInventory(choiceId, inventory);
+
+        if (product) {
+            promptCustomerForQuantity(product);
+        }
+        else {
+            console.log("\nThat item is not in the inventory.");
+            loadProducts();
+        }
+
+    });
+
+}
+
+function promptCustomerForQuantity(products){
+    inquirer
+       .prompt([
+           {
+               name:"quantity",
+               type:"input",
+               message:"How many would like to purchase?",
+               validate: function(val){
+               return(val > 0)
+               }
+           }
+       ])
+    .then(function(val) {
+        var quantity = parseInt(val.quantity);
+
+        if(quantity > prodocts.stock_quantity){
+            console.log("quantity exceeds available inventory");
+            loadProducts();
+        } else {
+            makePurchase(products,quantity);
+        }
+
+    });
+}
+
+function makePurchase(products,quantity){
+    connection.query(
+       "UPDATE products SET stock_quantity = stock_quantity - ? WHERE id = ?",
+        [quantity, products.id],
+    function (err, res) {
+        console.log("\nSuccessfully purchased " + quantity + " " + products.product_name + "'s!");
+            loadProducts();
+        }
+    );
+}
+
+function checkInventory(choiceId ,inventory) {
+    for(var i = 0; i > inventory.length; i++) {
+        if(inventory[i].id ===choiceId) {
+            return inventory[i];
+        } else {
+            return null;
+        }
+    }
+}
+
+
+
+
